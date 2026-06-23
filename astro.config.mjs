3ea14deb523c24c532d2c7c4bbb269cd5b5a6099
ooni <gdbsrjsdn@gmail.com>
2026-06-23 16:18:00 +0900

    style: update design system
    UI Layout
    font style
    view-transition
    header configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: { syntaxHighlight: false },
+  // Font 자체 호스팅
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Google Sans Code",
+      cssVariable: "--font-google-sans-code",
+      weights: [400, 500, 600, 700],
+      styles: ["normal"],
+    },
+  ],
+});
